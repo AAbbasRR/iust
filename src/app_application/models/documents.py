cee21f5890d2c72b81272ebd61109dafe9f8a936
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2023-07-29 20:24:54 +0330

    asd

--- src/app_application/models/documents.py
+++ src/app_application/models/documents.py
@@ -9,7 +9,6 @@ from app_user.models import UserModel
 from utils import GeneralDateModel
 from utils.validators import validate_image_file
 
-
 import os
 
 
@@ -43,70 +42,70 @@ class Document(GeneralDateModel):
     )
     curriculum_vitae = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Curriculum Vitae')
     )
     personal_photo = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Personal photo')
     )
     valid_passport = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Valid')
     )
     high_school_certificate = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('High School Certificate')
     )
     trans_script_high_school_certificate = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Trans Script High School Certificate')
     )
     bachelor_degree = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Bachelor Degree')
     )
     trans_script_bachelor_degree = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Trans Script Bachelor Degree')
     )
     master_degree = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Master Degree')
     )
     trans_script_master_degree = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Trans Script Master Degree')
     )
     supporting_letter = models.FileField(
         upload_to=document_image_directory_path,
-        validators=[validate_image_file,],
+        validators=[validate_image_file, ],
         null=True,
         blank=True,
         verbose_name=_('Supporting Letter')
