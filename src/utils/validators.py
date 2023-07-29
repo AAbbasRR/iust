9d6423732b41b763895e4289a3d03a8eadc2da80
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2023-07-29 20:53:02 +0330

    validators

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -4,19 +4,15 @@ from Abrat.settings import MAX_DOCUMENTS_SIZE
 
 from utils.base_errors import BaseErrors
 
-from PIL import Image
-
 
 def validate_image_file(value):
     if not value:
         return
 
-    image = Image.open(value)
-    image.verify()
-
     allowed_formats = ['JPEG', 'PNG', 'JPG', 'PDF']
-    if image.format not in allowed_formats:
+    file_extension = value.name.split('.')[-1].upper()
+    if file_extension not in allowed_formats:
         raise ValidationError(BaseErrors._change_error_variable("invalid_file_formats", formats=', '.join(allowed_formats)))
 
-    if len(value) > MAX_DOCUMENTS_SIZE:
+    if value.size > MAX_DOCUMENTS_SIZE:
         raise ValidationError(BaseErrors._change_error_variable("invalid_file_formats", size=MAX_DOCUMENTS_SIZE))
